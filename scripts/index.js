b866c4fefec6554ded11e801de918bfda4eb6702
May Donoban <rppride_2411@hotmail.com>
2024-08-31 11:54:07 -0500

    Validacion para los formularios

--- scripts/index.js
+++ scripts/index.js
@@ -8,9 +8,6 @@ editButton.addEventListener("click", function () {
   popup.classList.add("popup_show");
 
   let inputList = popup.querySelectorAll("input");
-  // inputList.forEach((item) => {
-  //   item.value = "May Donoban";
-  // });
   inputList[0].value = name.textContent;
   inputList[1].value = profession.textContent;
 });
@@ -40,7 +37,6 @@ const formButton = document.querySelector(".form__update");
 let closeFormButton = popupPlus.querySelector(".form__close");
 plusButton.addEventListener("click", function () {
   popupPlus.classList.add("form__show");
-  // let inputList = form.querySelectorAll("input");
 });
 
 closeFormButton.addEventListener("click", function () {
@@ -67,29 +63,9 @@ formButton.addEventListener("click", function (evt) {
   });
 });
 const handleDelete = (card) => {
-  console.log("carta");
   card.remove();
 };
 
-// const handleDelete = (card) => card.remove();
-// formButton.addEventListener("click", (evt) => {
-//   evt.preventDefault();
-
-//   const cardTemplate = document.querySelector(".content__element");
-//   const cardContainer = document.querySelector(".content__elements");
-//   const [titleValue, imageValue] = popupPlus
-//     .querySelectorAll("input")
-//     .map((input) => input.value);
-//   const card = cardTemplate.cloneNode(true);
-//   card.classList.remove("content__element-template");
-//   card.querySelector(".content__element-picture").src = imageValue;
-//   card.querySelector(".content__description-text").textContent = titleValue;
-//   card
-//     .querySelector(".content__bin")
-//     .addEventListener("click", () => handleDelete(card));
-//   cardContainer.prepend(card);
-//   popupPlus.classList.remove("form__show");
-// });
 const initialCards = [
   {
     name: "Valle de Yosemite",
@@ -116,30 +92,6 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
   },
 ];
-// initialCards.forEach((initialCard) => {
-//   const card = document.querySelector(".content__element").cloneNode(true);
-//   card.classList.remove("content__element-template");
-//   const cardContainer = document.querySelector(".content__elements");
-//   let inputList = popupPlus.querySelectorAll("input");
-//   const image = card.querySelector(".content__element-picture");
-//   const title = card.querySelector(".content__description-text");
-//   const trashIcon = card.querySelector(".content__bin");
-//   const likeButtons = card.querySelector(".content__description-like");
-//   likeButtons.addEventListener("click", () => likeHearth(card));
-//   const likeHearth = (card) => {
-//     if (likeButtons.classList.contains("content__description-like-focus")) {
-//       likeButtons.classList.remove("content__description-like-focus");
-//     } else {
-//       likeButtons.classList.add("content__description-like-focus");
-//     }
-//   };
-
-//   image.src = initialCard.link;
-//   title.textContent = initialCard.name;
-//   cardContainer.prepend(card);
-//   trashIcon.addEventListener("click", () => handleDelete(card));
-//   console.log(card);
-// });
 
 const likeHearth = (likeButtons) => {
   likeButtons.classList.toggle("content__description-like-focus");
@@ -179,6 +131,45 @@ function openPopupImage(title, link) {
   popupImageSrc.src = link;
 }
 popupImageClose.addEventListener("click", () => {
-  console.log("algunacosa");
   popupImage.classList.remove("popup_show");
+});
+
+function closeFormProfile(evt) {
+  const form = document.querySelector("#popup-profile");
+  closeEsc(evt, form);
+}
+
+function closeFormPlace(evt) {
+  const formPlace = document.querySelector("#popup-place");
+  closeEsc(evt, formPlace);
+}
+
+function closeCard(evt) {
+  const card = document.querySelector("#popup-image");
+  closeEsc(evt, card);
+}
+
+const closeEsc = (evt, elemnt) => {
+  if (evt.key === "Escape") {
+    elemnt.classList.remove("popup_show");
+    elemnt.classList.remove("form__show");
+  }
+};
+document.addEventListener("keydown", (evt) => {
+  closeFormPlace(evt);
+  closeFormProfile(evt);
+  closeCard(evt);
+  console.log("error");
+});
+
+const overlay = document.querySelectorAll(".popup__overlay");
+overlay.forEach((item) => {
+  item.addEventListener("click", () => {
+    const form = document.querySelector("#popup-profile");
+    const formPlace = document.querySelector("#popup-place");
+    const card = document.querySelector("#popup-image");
+    form.classList.remove("popup_show");
+    formPlace.classList.remove("form__show");
+    card.classList.remove("popup_show");
+  });
 });
---
+++ scripts/validate.js
@@ -0,0 +1,83 @@
+const showInputError = (formElement, inputElement, errorMessage, settings) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(settings.inputErrorClass);
+  errorElement.textContent = errorMessage;
+};
+
+const hideInputError = (formElement, inputElement, settings) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(settings.inputErrorClass);
+  errorElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement, settings) => {
+  if (!inputElement.validity.valid) {
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      settings
+    );
+  } else {
+    hideInputError(formElement, inputElement, settings);
+  }
+};
+const form = document.querySelector("#form-profile");
+const formPlace = document.querySelector("#form-place");
+const btn = document.querySelector("#btn");
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+function toggleButtonState(inputList, formButton) {
+  if (!hasInvalidInput(inputList)) {
+    formButton.removeAttribute("disabled");
+    formButton.classList.remove("popup__update-disabled");
+  } else {
+    formButton.setAttribute("disabled", true);
+    formButton.classList.add("popup__update-disabled");
+  }
+}
+
+const setEventListeners = (formElement, settings) => {
+  const inputList = Array.from(
+    formElement.querySelectorAll(settings.inputSelector)
+  );
+  const formButton = formElement.querySelector(".popup__update");
+  console.log(formButton);
+  toggleButtonState(inputList, formButton);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement, settings);
+      toggleButtonState(inputList, formButton);
+    });
+  });
+};
+
+const enableValidation = (settings) => {
+  const formList = Array.from(document.querySelectorAll(settings.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement, settings);
+  });
+};
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__item",
+  submitButtonSelector: ".popup__update",
+  inactiveButtonClass: "popup__update_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+});
